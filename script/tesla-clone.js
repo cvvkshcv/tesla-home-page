2eb3d1cf645c13b0b10e8d5c4e118da8c5c72fbc
Vikash UI_GEMS <cvvkshcv@gmail.com>
2021-09-05 02:24:33 +0530

    inital commit

---
+++ script/tesla-clone.js
@@ -0,0 +1,16 @@
+const sections = document.querySelector('.container');
+sections.onscroll = (e) => {
+  console.log(e.target.scrollTop);
+};
+const overlay = document.querySelector('.overlay');
+const overlayClose = document.querySelector('.overlay__close');
+
+const openMenu = document.querySelectorAll('.open-menu');
+openMenu.forEach(menuBtn => {
+  menuBtn.addEventListener('click', (e) => {
+    overlay.classList.add('show')
+  });
+});
+overlayClose.addEventListener('click', (e) => {
+  overlay.classList.remove('show')
+});
